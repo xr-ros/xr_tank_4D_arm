5a9eaf5a9cc9a7d89f1e456a9021e69c5fc3b64d
large-cat <large_cat_ro@163.com>
2020-10-28 17:02:00 +0800

    add install file

---
+++ install.sh
@@ -0,0 +1,36 @@
+project_path=$(cd `dirname $0`; pwd)
+project_name="${project_path##*/}"
+
+echo work_dir is $project_name
+
+if [ $project_name != "xr_tank_4D_arm" ]
+then
+	echo work in network mode
+	cd ~/Desktop
+
+	git clone https://gitee.com/xr-ros/xr_tank_4D_arm
+
+	if [ ! $! -eq 0 ]
+	then 
+		echo git failed, check your network.
+		exit -1
+	fi
+
+	cd xr_tank_4D_arm
+else
+	echo work in local mode
+fi
+
+if [ ! -d ~/moveit_ws/src ] 
+then
+	echo source dir not exsit, creat is now.
+	mkdir -p ~/moveit_ws/src
+fi
+
+echo cp sorce file
+
+cp -r tank_arm ~/moveit_ws/src/
+cp -r moveit_arm ~/moveit_ws/src/
+cp -r xr_arm_publisher/ ~/moveit_ws/src/
+
+echo install success
